5be73674341b567139c2e3912f0180fb864ba674
Kristan Frost <kristan.frost@gmail.com>
2019-12-10 13:52:37 -0600

    Add nessage

---
+++ VisualStudioDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VisualStudioDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Visual Studio Demo");
+            Console.ReadLine();
+
+        }
+    }
+}
